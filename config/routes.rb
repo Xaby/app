2f23d7a177f27aadc0ff999574cfa44d3daa2923
Xaby <xaby.tribe@gmail.com>
2011-01-30 14:55:19 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 App::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/hello"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
